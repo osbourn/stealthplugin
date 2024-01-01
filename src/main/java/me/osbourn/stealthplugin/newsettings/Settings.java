727ec8d6d9863d8121a5dc74274eb7b32787828d
Niels Voss <code@nielsvoss.com>
2023-12-31 23:04:35 -0800

    Add more settings to Settings class

--- src/main/java/me/osbourn/stealthplugin/newsettings/Settings.java
+++ src/main/java/me/osbourn/stealthplugin/newsettings/Settings.java
@@ -1,9 +1,8 @@
 package me.osbourn.stealthplugin.newsettings;
 
-import me.osbourn.stealthplugin.settingsapi.BooleanSetting;
-import me.osbourn.stealthplugin.settingsapi.IntegerSetting;
-import me.osbourn.stealthplugin.settingsapi.StringSetting;
+import me.osbourn.stealthplugin.util.NullableBlockPosition;
 
+@SuppressWarnings("unused")
 public final class Settings {
     private Settings() {
     }
@@ -28,4 +27,18 @@ public final class Settings {
     public static String attackingTeamName = "red";
     @Setting(name = "defendingTeamName")
     public static String defendingTeamName = "blue";
+    @Setting(name = "attackingTeamSpawnLocation")
+    public static NullableBlockPosition attackingTeamSpawnLocation = NullableBlockPosition.UNSET;
+    @Setting(name = "defendingTeamSpawnLocation")
+    public static NullableBlockPosition defendingTeamSpawnLocation = NullableBlockPosition.UNSET;
+    @Setting(name = "attackingTeamChestLocation")
+    public static NullableBlockPosition attackingTeamChestLocation = NullableBlockPosition.UNSET;
+    @Setting(name = "defendingTeamChestLocation")
+    public static NullableBlockPosition defendingTeamChestLocation = NullableBlockPosition.UNSET;
+    @Setting(name = "respawnLocation")
+    public static NullableBlockPosition respawnLocation = NullableBlockPosition.UNSET;
+    @Setting(name = "lobbyLocation")
+    public static NullableBlockPosition lobbyLocation = NullableBlockPosition.UNSET;
+    @Setting(name = "numberOfTargets")
+    public static int numberOfTargets = 2;
 }
