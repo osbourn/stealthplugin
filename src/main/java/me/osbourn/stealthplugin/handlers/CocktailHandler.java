1688dd3376380eed699489a21de1f0c682ac61de
Niels Voss <code@nielsvoss.com>
2024-01-03 23:16:51 -0800

    Make cocktail create fire when fuse is up

--- src/main/java/me/osbourn/stealthplugin/handlers/CocktailHandler.java
+++ src/main/java/me/osbourn/stealthplugin/handlers/CocktailHandler.java
@@ -1,9 +1,10 @@
 package me.osbourn.stealthplugin.handlers;
 
 import org.bukkit.*;
-import org.bukkit.entity.Entity;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.ThrownPotion;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -60,20 +61,21 @@ public class CocktailHandler implements Listener {
                 newPotion.setItemMeta(newPotionMeta);
                 player.getInventory().setItem(slot, newPotion);
 
-                Bukkit.getScheduler().runTaskLater(this.plugin, () -> onFuseEnd(cocktailId), 60);
+                Bukkit.getScheduler().runTaskLater(this.plugin, () -> onFuseRunOut(cocktailId), 60);
                 return true;
             }
         }
         return false;
     }
 
-    private void onFuseEnd(long cocktailId) {
+    private void onFuseRunOut(long cocktailId) {
         for (Player player : Bukkit.getOnlinePlayers()) {
             for (int slot = 0; slot < player.getInventory().getSize(); slot++) {
                 ItemStack itemStack = player.getInventory().getItem(slot);
                 if (matchesCocktailId(itemStack, cocktailId)) {
                     player.setFireTicks(200);
                     player.getInventory().setItem(slot, null);
+                    createFireCylinder(player.getLocation(), 1, 0);
                 }
             }
         }
@@ -81,7 +83,13 @@ public class CocktailHandler implements Listener {
         for (World world : Bukkit.getWorlds()) {
             for (Item itemEntity : world.getEntitiesByClass(Item.class)) {
                 if (matchesCocktailId(itemEntity.getItemStack(), cocktailId)) {
+                    createFireCylinder(itemEntity.getLocation(), 1, 0);
                     itemEntity.remove();
+                }
+            }
+            for (ThrownPotion thrownPotion : world.getEntitiesByClass(ThrownPotion.class)) {
+                if (matchesCocktailId(thrownPotion.getItem(), cocktailId)) {
+                    createFireCylinder(thrownPotion.getLocation(), 5, 5);
                 }
             }
         }
@@ -96,6 +104,27 @@ public class CocktailHandler implements Listener {
             }
         }
         return false;
+    }
+
+    private void createFireCylinder(Location center, int radius, int heightRadius) {
+        if (radius < 0 || heightRadius < 0) throw new IllegalArgumentException();
+        World world = center.getWorld();
+        if (world == null) throw new IllegalArgumentException();
+        for (int x = center.getBlockX() - radius; x <= center.getBlockX() + radius; x++) {
+            for (int y = center.getBlockY() - heightRadius; y <= center.getBlockY() + heightRadius; y++) {
+                for (int z = center.getBlockZ() - radius; z <= center.getBlockZ() + radius; z++) {
+                    int deltaX = center.getBlockX() - x;
+                    int deltaZ = center.getBlockZ() - z;
+                    int distanceSquared = deltaX * deltaX + deltaZ * deltaZ;
+                    if (distanceSquared <= radius * radius) {
+                        if (world.getBlockAt(x, y, z).getType().isAir()) {
+                            BlockData blockData = Material.FIRE.createBlockData();
+                            world.setBlockData(x, y, z, blockData);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     @EventHandler
